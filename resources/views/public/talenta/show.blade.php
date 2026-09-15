fa4f9ea39a741c5d0a82fa21604db78c667a9eee
RayhanAfif-del <muhammadrayhan955@gmail.com>
2026-09-15 12:01:46 +0700

    hapus teks

--- resources/views/public/talenta/show.blade.php
+++ resources/views/public/talenta/show.blade.php
@@ -264,10 +264,6 @@
                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"/></svg>
                     Kembali ke Direktori Talenta
                 </a>
-                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"/></svg>
-                    Kembali ke Direktori Talenta
-                </a>
-
             </aside>
         </div>
     </main>
